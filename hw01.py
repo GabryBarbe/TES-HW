8b8af420eb7f477d5093149cd93e489e4547d88a
Gabriele Barbero <barbero.gabriele03@gmail.com>
2024-11-27 18:25:18 +0100

    +calcolo_fft_libreria +plot_fft

--- hw01.py
+++ hw01.py
@@ -16,7 +16,7 @@ def plot_waveform(rate, data):
 
 def divisione_audio(rate, data, M):
     campioni_per_sezione = rate*M # numero di campioni per sezione
-    num_sezioni = len(data)//campioni_per_sezione # numero di sezioni
+    num_sezioni = (len(data)//campioni_per_sezione) + 1# numero di sezioni
     
     sezioni = []
     for i in range(num_sezioni):
@@ -26,7 +26,24 @@ def divisione_audio(rate, data, M):
 
     return sezioni
 
-    
+def plot_fft(freq, ampiezza, num_segmento):
+    freq_pos = freq[freq > 0]
+    ampiezza_pos = ampiezza[freq > 0]
+
+    plt.plot(freq_pos, ampiezza_pos)
+    plt.xlabel("Frequenza")
+    plt.ylabel("Ampiezza")
+    plt.suptitle("Segmento " + str(num_segmento))
+    plt.grid(True)
+    plt.show()
+
+def calcolo_fft_libreria(rate, segmenti):
+    for i,segmento in enumerate(segmenti):
+        fft_segmento = sp.fft.fft(segmento)
+        freq_segmento = sp.fft.fftfreq(len(segmento), d=1/rate)
+        ampiezza_segmento = np.abs(fft_segmento)
+        plot_fft(freq_segmento, ampiezza_segmento, i+1)
+        
 
 def main():
     # rate è la frequenza di campionamento
@@ -39,7 +56,7 @@ def main():
     # sd.play(data, rate)  # riproduce il file audio
     # sd.wait() # attende la fine esecuzione del file audio
     
-    sezioni = divisione_audio(rate, data, 5)
+    sezioni = divisione_audio(rate, data, 15)
 
     # riproduzione dei segmenti di M secondi
     # for i in range(len(segmenti)):
@@ -47,6 +64,8 @@ def main():
     #     sd.wait()   
 
     #plot_waveform(rate, data)
+
+    calcolo_fft_libreria(rate, sezioni)
 
     return 0
 
